8d218314e9172b4a40b1fc38f0d70014ccb9d8fd
Davicous <dnepwns@gmail.com>
2022-02-02 22:55:40 -0500

    Version 2.0

--- js/main.js
+++ js/main.js
@@ -1,36 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
-  getNewWord();
+  //getNewWord();
 
   let guessedWords = [[]];
   let availableSpace = 1;
 
-  let word;
+  let word = "dumpy";
   let guessedWordCount = 0;
 
   const keys = document.querySelectorAll(".keyboard-row button");
-
-  function getNewWord() {
-    fetch(
-      `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        word = res.word;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
 
   function getCurrentWordArr() {
     const numberOfGuessedWords = guessedWords.length;
@@ -71,50 +49,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentWordArr = getCurrentWordArr();
     if (currentWordArr.length !== 5) {
       window.alert("Word must be 5 letters");
+	  return;
     }
+	
+	const firstLetterId = guessedWordCount * 5 + 1;
+    const interval = 200;
+	currentWordArr.forEach((letter, index) => {
+	  setTimeout(() => {
+		const tileColor = getTileColor(letter, index);
 
-    const currentWord = currentWordArr.join("");
-
-    fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error();
-        }
-
-        const firstLetterId = guessedWordCount * 5 + 1;
-        const interval = 200;
-        currentWordArr.forEach((letter, index) => {
-          setTimeout(() => {
-            const tileColor = getTileColor(letter, index);
-
-            const letterId = firstLetterId + index;
-            const letterEl = document.getElementById(letterId);
-            letterEl.classList.add("animate__flipInX");
-            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-          }, interval * index);
-        });
-
-        guessedWordCount += 1;
-
-        if (currentWord === word) {
-          window.alert("Congratulations!");
-        }
-
-        if (guessedWords.length === 6) {
-          window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
-        }
-
-        guessedWords.push([]);
-      })
-      .catch(() => {
-        window.alert("Word is not recognised!");
-      });
+		const letterId = firstLetterId + index;
+		const letterEl = document.getElementById(letterId);
+		letterEl.classList.add("animate__flipInX");
+		letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+	  }, interval * index);
+	});
+	
+	setTimeout(() => {
+		const currentWord = currentWordArr.join("");
+	
+		if(currentWord === word) {
+			window.alert(`Yay! ${word}!`);
+		}
+		else {
+			window.alert(`Excuse me! The word is ${word}, silly!`);
+		}
+	}, interval * 6);
+	
+	guessedWordCount += 1;
   }
 
   function createSquares() {
